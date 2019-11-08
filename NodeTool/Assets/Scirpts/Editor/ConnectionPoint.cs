e8ec153d854ad44b4186dbb4279d758789f1901f
Alexis <thiebautalexis01@gmail.com>
2019-11-08 18:48:23 +0100

    Creating Classes

---
+++ NodeTool/Assets/Scirpts/Editor/ConnectionPoint.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+public class ConnectionPoint
+{
+    #region Fields and Properties
+    public Rect ConnectionPointRect { get; private set; }
+    public ConnectionPointType Type { get; private set; } = ConnectionPointType.None;
+    public Node Node { get; private set;  }
+    public GUIStyle ConnectionPointStyle { get; private set; }
+    public Action<ConnectionPoint> OnClickOnConnectionPoint { get; private set; } = null;
+    #endregion
+
+    #region Constructor 
+    public ConnectionPoint(Node _node, ConnectionPointType _type, GUIStyle _style, Action<ConnectionPoint> _onClickAction)
+    {
+        Node = _node;
+        Type = _type;
+        ConnectionPointStyle = _style;
+        OnClickOnConnectionPoint = _onClickAction;
+        ConnectionPointRect = new Rect(0, 0, 10.0f, 20.0f); 
+    }
+    #endregion
+
+    #region Methods
+    public void Draw()
+    {
+        Rect _r = ConnectionPointRect; 
+        _r.y = Node.NodeRect.position.y + (Node.NodeRect.height * 0.5f) - ConnectionPointRect.height * 0.5f;
+
+        switch (Type)
+        {
+            case ConnectionPointType.In:
+                _r.x = Node.NodeRect.x - ConnectionPointRect.width + 8f;
+                break;
+
+            case ConnectionPointType.Out:
+                _r.x = Node.NodeRect.x + Node.NodeRect.width - 8f;
+                break;
+        }
+
+        ConnectionPointRect = _r; 
+        if (GUI.Button(ConnectionPointRect, "" /*, ConnectionPointStyle*/))
+        {
+            if (OnClickOnConnectionPoint != null)
+            {
+                OnClickOnConnectionPoint(this);
+            }
+        }
+    }
+    #endregion
+}
+
+public enum ConnectionPointType
+{
+    None, 
+    In, 
+    Out
+}
---
+++ NodeTool/Assets/Scirpts/Editor/Node.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+
+public class Node
+{
+    #region Fields and Properties
+    public Rect NodeRect { get; private set; }
+    public string NodeTitle { get; private set; }
+    public GUIStyle NodeStyle { get; private set; }
+    public GUIStyle InPointStyle  { get; private set; }
+    public GUIStyle OutPointStyle { get; private set; }
+
+    public ConnectionPoint InPoint { get; private set; }
+    public ConnectionPoint OutPoint { get; private set; }
+
+    private bool m_isDragged = false;
+    #endregion
+
+    #region Constructor
+    /// <summary>
+    /// Init the Node with a rect at _position with a _width and _height and a _nodeStyle 
+    /// </summary>
+    /// <param name="_position">Position of the node</param>
+    /// <param name="_width">Width of the node</param>
+    /// <param name="_height">Height of the node</param>
+    /// <param name="_nodeStyle">GUI Style of the node</param>
+    public Node(Vector2 _position, float _width, float _height, GUIStyle _nodeStyle, Action<ConnectionPoint> _onClickInPoint, Action<ConnectionPoint> _onClickOutPoint)
+    {
+        NodeRect = new Rect(_position.x, _position.y, _width, _height);
+        NodeStyle = _nodeStyle;
+        InPoint = new ConnectionPoint(this, ConnectionPointType.In, InPointStyle, _onClickInPoint);
+        OutPoint = new ConnectionPoint(this, ConnectionPointType.Out, OutPointStyle, _onClickOutPoint); 
+    }
+    #endregion 
+
+    #region Methods
+    /// <summary>
+    /// Move the position of the Node of the delta
+    /// </summary>
+    /// <param name="_delta">Where to move the node position</param>
+    public void Drag(Vector2 _delta)
+    {
+        Rect _r = new Rect(NodeRect.position + _delta, NodeRect.size);
+        NodeRect = _r; 
+    }
+
+    /// <summary>
+    /// Draw the Node
+    /// </summary>
+    public void Draw()
+    {
+        InPoint.Draw();
+        OutPoint.Draw();
+        GUI.Box(NodeRect, NodeTitle, NodeStyle); 
+    }
+
+    public bool ProcessEvent(Event _e)
+    {
+        switch (_e.type)
+        {
+            case EventType.MouseDown:
+                if (_e.button == 0)
+                {
+                    if (NodeRect.Contains(_e.mousePosition))
+                    {
+                        m_isDragged = true;
+                        GUI.changed = true;
+                    }
+                    else
+                    {
+                        GUI.changed = true;
+                    }
+                }
+                else
+                    m_isDragged = false; 
+                break;
+            case EventType.MouseUp:
+                m_isDragged = false;
+                break;
+            case EventType.MouseDrag:
+                if(_e.button == 0 && m_isDragged)
+                {
+                    Drag(_e.delta);
+                    _e.Use();
+                    return true; 
+                }
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+    #endregion
+}
